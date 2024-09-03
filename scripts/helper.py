72547cd050477e191f241e77dbbec9c26d0bc601
Alex Andrade <alexander.andrade@gmail.com>
2024-09-03 08:20:00 -0500

    🔧 setup the reporting

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,11 +1,11 @@
 import subprocess
+import shutil
+import os
 
 
 def post_install():
     print("Running post-install actions...")
-    subprocess.run(
-        ["poetry", "run", "pre-commit", "install"], check=True
-    )  # Use list and check=True
+    subprocess.run("poetry run pre-commit install", shell=True, check=False)
     subprocess.run("npm install", shell=True, check=False)
 
 
@@ -28,6 +28,46 @@ def test(headless=False):
     print("Running tests...")
     environment = "HEADLESS=" + str(headless)
     subprocess.run(environment + " poetry run behave", shell=True, check=False)
+
+
+def report():
+    print("Generating report...")
+    main_dir = "./.run/reports"
+    results_output_dir = main_dir + "/behave"
+    reports_output_dir = main_dir + "/allure"
+    history_name_dir = "/history"
+
+    # Remove history folder
+    if os.path.exists(results_output_dir + history_name_dir):
+        shutil.rmtree(results_output_dir + history_name_dir)
+
+    # Copy history folder
+    if os.path.exists(reports_output_dir + history_name_dir):
+        shutil.copytree(
+            reports_output_dir + history_name_dir, results_output_dir + history_name_dir
+        )
+
+    subprocess.run(
+        "npx allure generate "
+        + results_output_dir
+        + " --clean -o "
+        + reports_output_dir,
+        shell=True,
+        check=False,
+    )
+
+    print("Report generated on: " + reports_output_dir + "/index.html")
+    current_directory = subprocess.run(
+        "pwd", capture_output=True, text=True, check=False
+    )
+    current_directory = current_directory.stdout.strip()
+    print(
+        'You can open the report with: open -a "Google Chrome" --args '
+        '--allow-file-access-from-files "'
+        + current_directory
+        + reports_output_dir[1:]
+        + '/index.html"'
+    )
 
 
 def test_ci():
